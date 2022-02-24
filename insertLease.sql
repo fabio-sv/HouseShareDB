b2e75e234086bc61e3ed41028d099a2184e5a1b8
Tiaan van der Riel <tiaanvdr@bbd.co.za>
2022-02-24 12:06:25 +0200

    Insert tenant and lease

---
+++ insertLease.sql
@@ -0,0 +1,26 @@
+Use HouseShare
+Go 
+
+INSERT INTO [Lease]
+VALUES
+ (1, 'Leases/Lease_1.pdf');
+ (2, 'Leases/Lease_2.pdf');
+ (3, 'Leases/Lease_3.pdf');
+ (4, 'Leases/Lease_4.pdf');
+ (5, 'Leases/Lease_5.pdf');
+ (6, 'Leases/Lease_6.pdf');
+ (7, 'Leases/Lease_7.pdf');
+ (8, 'Leases/Lease_8.pdf');
+ (9, 'Leases/Lease_9.pdf');
+ (10, 'Leases/Lease_10.pdf');
+ (11, 'Leases/Lease_11.pdf');
+ (12, 'Leases/Lease_12.pdf');
+ (13, 'Leases/Lease_13.pdf');
+ (14, 'Leases/Lease_14.pdf');
+ (15, 'Leases/Lease_15.pdf');
+ (16, 'Leases/Lease_16.pdf');
+ (17, 'Leases/Lease_17.pdf');
+ (18, 'Leases/Lease_18.pdf');
+ (19, 'Leases/Lease_19.pdf');
+ (20, 'Leases/Lease_20.pdf');
+GO
